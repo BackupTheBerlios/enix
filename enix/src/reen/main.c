56ea30ac1ad0822a592124242ef14e182ba15f4c
dooh <dooh>
2004-07-28 13:53:15 +0000

    make scaling optional

--- enix/src/reen/main.c
+++ enix/src/reen/main.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: main.c,v 1.6 2004/07/28 12:47:47 dooh Exp $
+ * $Id: main.c,v 1.7 2004/07/28 13:53:15 dooh Exp $
  *
  * re-encoder main
  */
@@ -70,7 +70,7 @@ int main(int argc, char* argv[]) {
   bitrate_video = 150000;
   twopass       = 0;
   bframes       = -1;
-  width         = 320;
+  width         = 0;
   verbosity     = 0;
 
   /*
@@ -119,9 +119,12 @@ int main(int argc, char* argv[]) {
    * open an source stream chain
    */
 
-  xine_stream   = enix_xine_stream_new (infile, 1, 1);
-  stream        = enix_scaler_new (xine_stream, width, ENIX_SCALER_MODE_AR_KEEP, 16);
-
+  stream = enix_xine_stream_new (infile, 1, 1);
+  if (width) {
+    xine_stream=stream;
+    stream = enix_scaler_new (xine_stream, width, ENIX_SCALER_MODE_AR_KEEP, 16);
+  }
+    
   /*
    * create an encoder
    */
